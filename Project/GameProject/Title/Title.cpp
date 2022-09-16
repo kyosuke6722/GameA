cbb9f4cd6be68cba368f5c6297615641c2c7927f
ryou-anubuki <A2oo3o812r>
2022-09-16 14:42:52 +0900

    タイトル作りました

--- Project/GameProject/Title/Title.cpp
+++ Project/GameProject/Title/Title.cpp
@@ -1,13 +1,34 @@
 #include"Title.h"
+#include "../Game/Game.h"
 
-Title::Title():Base(eType_Scene) {
+
+Title::Title() :Base(eType_Scene),
+m_title_text("C:\\Windows\\Fonts\\msgothic.ttc", 64)
+{
+	m_img = COPY_RESOURCE("Title", CImage);
 }
 
-Title::~Title(){
+Title::~Title()
+{
+	//全てのオブジェクトを破棄
+	Base::KillAll();
+	//ゲームシーンへ
+	Base::Add(new Game());
+}
+void Title::Update()
+{
+	//ボタン１でタイトル破棄
+	if (PUSH(CInput::eButton1)) {
+		SetKill();
+	}
 }
 
-void Title::Update(){
+void Title::Draw()
+{
+	m_img.Draw();
+	//文字表示
+	m_title_text.Draw(64, 256, 0, 0, 0, "Title");
+	m_title_text.Draw(64, 512, 0, 0, 0, "Push Z");
 }
 
-void Title::Draw(){
-}
+
