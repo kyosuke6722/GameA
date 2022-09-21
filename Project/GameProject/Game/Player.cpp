12871b71d6c1fe847f18e74f1ff9b4f79f8787a8
TAKU415 <th.mzk415@gmail.com>
2022-09-21 12:44:07 +0900

    player

--- Project/GameProject/Game/Player.cpp
+++ Project/GameProject/Game/Player.cpp
@@ -1,9 +1,10 @@
 #include "Player.h"
+#include "Bullet.h"
+#include "Game.h"
 
 Player::Player(const CVector2D& pos)
 	:Base(eType_Player)
 {
-	//m_img.Load("Image/Player.png");
 	m_img = COPY_RESOURCE("Player", CImage);
 	m_pos = pos;
 	//中心を設定
@@ -17,12 +18,23 @@ void Player::Update()
 	//回転値を逆算
 	m_ang = atan2(vec.x, vec.y);
 
+	if (PUSH(CInput::eMouseL)) {
+		Base::Add(new Bullet(eType_Player_Bullet, m_pos, m_ang, 4));
+	}
+
+
 }
 void Player::Draw()
 {
-	m_img.SetPos(600, 300);
+	//位置
+	m_img.SetPos(m_pos);
+	
+	//サイズ
 	m_img.SetSize(40, 40);
+	
+	//アングル
 	m_img.SetAng(m_ang);
+
 	m_img.Draw();
 }
 void Player::Collision(Base* b)
